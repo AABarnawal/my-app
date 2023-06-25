1e5d2e45fecd4215c19ab48085c15d9ae4488399
Ankit Kumar <66714860+AABarnawal@users.noreply.github.com>
2023-06-25 07:49:38 +0530

    Update Sdata.jsx

--- src/Sdata.jsx
+++ src/Sdata.jsx
@@ -2,35 +2,93 @@ import React from "react";
 
 const Sdata = [
     {
+        id: 9,
+        imge : "https://static.toiimg.com/photo/msid-100953198/Jee%20Karda%20Season%201.jpg?100953198",
+        title : "JEE KARDA",
+        ott: "AMAZON",
+        lnk : "https://www.primevideo.com/detail/Jee-Karda/0SG8AMA4RU67QK83GPSHXBR157",
+    },
+   
+    {
+        id: 1,
         imge : "https://m.media-amazon.com/images/M/MV5BZjUyZjNmMTMtMTZmMS00N2FjLWJlZGUtMWM5OWE2MjdlOGU0XkEyXkFqcGdeQXVyOTI3MzI4MzA@._V1_.jpg",
         title : "KHAKHEE",
+        ott: "NETFLIX",
         lnk : "https://www.netflix.com/in/title/81290872",
     },
     {
+        id: 10,
+        imge : "https://static-koimoi.akamaized.net/wp-content/new-galleries/2020/11/top-amazon-prime-series-romantic-dramas-to-beat-your-pandemic-blues-002.jpg",
+        title : "VAMPIRE DIARIS",
+        ott: "AMAZON",
+        lnk : "https://www.primevideo.com/detail/The-Vampire-Diaries/0HG2QOPONEONZPP2ZXNMA6M8Q7",
+    },
+    {
+        id: 2,
         imge : "https://m.media-amazon.com/images/M/MV5BOGVmMGYwZTEtOGFlYS00ODRhLTg3M2MtMzc1OGNhNzU3N2IzXkEyXkFqcGdeQXVyNDY4NjAxNTc@._V1_.jpg",
         title : "KOTA FACTORY",
+        ott: "NETFLIX",
         lnk : "https://www.netflix.com/title/81249783",
     },
     {
+        id: 3,
         imge : "https://m.media-amazon.com/images/M/MV5BOTUzMTMzNTgtNmI5NS00ZTI1LTgzMjYtZTFkZWZkYTE0MWMyXkEyXkFqcGdeQXVyNjY1MTg4Mzc@._V1_.jpg",
         title : "MISMATCHED",
+        ott: "NETFLIX",
         lnk : "https://www.netflix.com/title/80220079",
     },
     {
+        id: 11,
+        imge : "https://m.media-amazon.com/images/I/517EHUjK7GL.jpg",
+        title : "MY FAULT",
+        ott: "AMAZON",
+        lnk : "https://www.primevideo.com/detail/My-Fault/0S0OIMOTSETYIWA2EVTWWH7JR9",
+    },
+    {
+        id: 4,
         imge : "https://dnm.nflximg.net/api/v6/WNk1mr9x_Cd_2itp6pUM7-lXMJg/AAAABQTbU9GTguBcmHW7osa8Egtx2BAd5E_Kx-j1QAB0TUhp9UtvTWuZsq1FwbwKwrBvZQWVWnY98L-eAXjweyLKspMkd1dZz0-dvrPF1JCSGAlb96ejSxYGwZ4Q2tqgHUktLvEV_w.jpg?r=6c0",
         title : "COBRA KAI",
+        ott: "NETFLIX",
         lnk : "https://www.netflix.com/title/81002370",
     },
     {
+        id: 12,
+        imge : "https://static-koimoi.akamaized.net/wp-content/new-galleries/2020/11/top-amazon-prime-series-romantic-dramas-to-beat-your-pandemic-blues-005.jpg",
+        title : "MADE IN HEAVEN",
+        ott: "AMAZON",
+        lnk : "https://www.primevideo.com/detail/Made-In-Heaven/0JP36WERGLAJLNET3SZS9XTXJ1",
+    },
+    {
+        id: 5,
         imge : "https://m.media-amazon.com/images/M/MV5BOTY1ZGM2YzQtMTBjZC00NjE2LWJlNzUtYjA0YjYxNzBjMmRjXkEyXkFqcGdeQXVyMTEzMTI1Mjk3._V1_FMjpg_UX1000_.jpg",
-        title : "ALL OF US ARE DEAD",
+        title : "ALL OF US DEAD",
+        ott: "NETFLIX",
         lnk : "https://www.netflix.com/title/81237994",
     },
     {
+        id: 6,
         imge : "https://upload.wikimedia.org/wikipedia/en/d/dd/Squid_Game.jpg",
         title : "SQUID GAME",
+        ott: "NETFLIX",
         lnk : "https://www.netflix.com/title/81040344",
     },
+    {
+        id: 7,
+        imge : "https://www.scrolldroll.com/wp-content/uploads/2022/06/Panchayat-2-Amazon-prime-Hindi-web-series-of-2022-scaled.jpg",
+        title : "PANCHAYAT",
+        ott: "AMAZON",
+        lnk : "https://www.primevideo.com/detail/Panchayat/0KEP4A6DWRKFYQFTSU5RXHEAN2",
+    },
+    {
+        id: 8,
+        imge : "https://m.media-amazon.com/images/M/MV5BN2NlNGYwYTUtMTczMi00NGVjLTgwMzUtNjBkZjIyNDc2NjcxXkEyXkFqcGdeQXVyODQ5NDUwMDk@._V1_FMjpg_UX1000_.jpg",
+        title : "MIRZAPUR",
+        ott: "AMAZON",
+        lnk : "https://www.primevideo.com/detail/Mirzapur/0PDOKMV9CRLOMO5EUKNCUJLG4Q",
+    },
+  
+   
+   
 ]
 
 export default Sdata;
